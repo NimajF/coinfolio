5ad91d3e08fcef2e352d0280fe726393df65dd71
unknown <benjafran2000@gmail.com>
2024-11-20 19:15:52 -0300

    Update User model

--- src/models/user.js
+++ src/models/user.js
@@ -3,20 +3,21 @@ import mongoose, { model } from "mongoose";
 const transactionSchema = new mongoose.Schema({
   amount: { type: Number, required: true },
   price: { type: Number, required: true },
+  transactionInversion: { type: Number, required: true, default: 0 },
   date: { type: Date, required: true },
 });
 
+const coinSchema = new mongoose.Schema({
+  symbol: { type: String, required: true },
+  coinId: { type: String, required: true },
+  coinImage: { type: String, required: true },
+  transactions: [transactionSchema],
+  totalAmount: { type: Number, default: 0 },
+  totalCoinInversion: { type: Number, default: 0 },
+});
+
 const portfolioSchema = new mongoose.Schema({
-  coins: {
-    btc: {
-      transactions: [transactionSchema],
-      totalAmount: { type: Number, default: 0 },
-    },
-    eth: {
-      transactions: [transactionSchema],
-      totalAmount: { type: Number, default: 0 },
-    },
-  },
+  coins: [coinSchema],
   totalInvestment: { type: Number, default: 0 },
 });
 
@@ -36,7 +37,19 @@ const userSchema = new mongoose.Schema({
     required: [true, "Password is required"],
     select: false,
   },
-  portfolio: portfolioSchema,
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  portfolio: {
+    type: portfolioSchema,
+    default: () => ({}),
+  },
+  favorites: {
+    type: Array,
+    required: false,
+    default: [],
+  },
 });
 
 export default mongoose.models.User || mongoose.model("User", userSchema);
